1f3e0b65462e1e29bfe6d59e656c8e4919e1404e
Christian Dupuis <cd@atomist.com>
2018-06-20 15:12:48 +0200

    Initial commit from Atomist

---
+++ src/main/java/com/spring/Cd28Application.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd28Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Cd28Application.class, args);
+	}
+}
---
+++ src/main/java/com/spring/Cd28Controller.java
@@ -0,0 +1,22 @@
+package com.spring;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+class Cd28Controller {
+
+    @RequestMapping(method = GET, path = "/")
+    public String root() {
+        return "Hello Kubernetes demo! I'm served by " + getClass().getName();
+    }
+
+    @RequestMapping(method = GET, path = "hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
